2525e7a0f39b63e9d91df33eae4ba4c5a15e9e61
ulsaya04 <124543635+ulsaya04@users.noreply.github.com>
2025-04-28 20:46:32 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,40 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
+document.getElementById("tg-form").addEventListener("submit", function(e) {
     e.preventDefault();
-  
-    const fullname = document.getElementById("fullname").value;
+
+    const name = document.getElementById("name").value;
     const phone = document.getElementById("phone").value;
     const email = document.getElementById("email").value;
-    const service = document.getElementById("service").value;
-  
+    const messageText = document.getElementById("message").value;
+
     const message = `
-  📬 Новая заявка с сайта Logitex:
-  👤 Имя: ${fullname}
-  📞 Телефон: ${phone}
-  📧 Email: ${email}
-  🛠 Услуга: ${service}
+📬 Новая заявка с сайта Logitex:
+👤 Имя: ${name}
+📞 Телефон: ${phone}
+📧 Email: ${email}
+💬 Сообщение: ${messageText}
     `;
-  
+
     const token = "7604871085:AAE1E3Em4K9Qjiy3R4OoqBoNfT3USQAGy8s";
     const chat_id = "1603020730";
-  
+
     fetch(`https://api.telegram.org/bot${token}/sendMessage`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        chat_id: chat_id,
-        text: message
-      })
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+            chat_id: chat_id,
+            text: message
+        })
     })
     .then(response => {
-      if (response.ok) {
-        document.getElementById("status").innerText = "✅ Заявка успешно отправлена!";
-        document.getElementById("contactForm").reset();
-      } else {
-        document.getElementById("status").innerText = "❌ Ошибка при отправке. Попробуйте позже.";
-      }
+        if (response.ok) {
+            document.getElementById("status-msg").innerText = "✅ Заявка успешно отправлена!";
+            document.getElementById("tg-form").reset();
+        } else {
+            document.getElementById("status-msg").innerText = "❌ Ошибка при отправке. Попробуйте позже.";
+        }
     })
     .catch(error => {
-      console.error("Ошибка:", error);
-      document.getElementById("status").innerText = "❌ Сетевая ошибка.";
+        console.error("Ошибка:", error);
+        document.getElementById("status-msg").innerText = "❌ Сетевая ошибка.";
     });
-  });
-  
+});
